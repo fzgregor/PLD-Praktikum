ad83493248e4b0c51a4fe4a0544482a7d87fd994
rotschopf <rentafranz@gmail.com>
2012-05-27 22:05:30 +0200

    did exercise 1 task 3

---
+++ exercise_1/task_3/taktteiler.vhd
@@ -0,0 +1,64 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:15:23 05/27/2012 
+-- Design Name: 
+-- Module Name:    taktteiler - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity taktteiler is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+           enable_clock : in  STD_LOGIC;
+           clks : out  STD_LOGIC_VECTOR (7 downto 0));
+end taktteiler;
+
+architecture Behavioral of taktteiler is
+	 signal cnt : unsigned(20 downto 0) := (others => '0');
+begin
+
+clks(7) <= cnt(20);
+clks(6) <= cnt(19);
+clks(5) <= cnt(18);
+clks(4) <= cnt(17);
+clks(3) <= cnt(16);
+clks(2) <= cnt(15);
+clks(1) <= cnt(14);
+clks(0) <= cnt(13);
+
+process(clk, rst, enable_clock)
+begin
+    if rst = '1' then
+	     cnt <= (others => '0');
+    elsif rising_edge(clk) and enable_clock = '1' then
+	     cnt <= cnt + 1;
+	 end if;
+end process;
+
+
+end Behavioral;
+
---
+++ exercise_1/task_3/taktteiler_test.vhd
@@ -0,0 +1,98 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   18:25:15 05/27/2012
+-- Design Name:   
+-- Module Name:   /home/live/workspace/pld/exercise_1/task_3/taktteiler_test.vhd
+-- Project Name:  task_3
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: taktteiler
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY taktteiler_test IS
+END taktteiler_test;
+ 
+ARCHITECTURE behavior OF taktteiler_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT taktteiler
+    PORT(
+         clk : IN  std_logic;
+         rst : IN  std_logic;
+         enable : IN  std_logic;
+         clks : OUT  std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal rst : std_logic := '0';
+   signal enable : std_logic := '1';
+
+ 	--Outputs
+   signal clks : std_logic_vector(7 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: taktteiler PORT MAP (
+          clk => clk,
+          rst => rst,
+          enable => enable,
+          clks => clks
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for clk_period*10;
+		
+		rst <= '1';
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
