9e8649a27f2821ba02e3723b8e688f7aa1813da3
rotschopf <rentafranz@gmail.com>
2012-09-27 12:46:48 +0200

    added seperate directory for ex 2 task 1 d

---
+++ exercise_2/task_1_d/state_machine.vhd
@@ -0,0 +1,53 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity state_machine is
+    Port ( clk : in  STD_LOGIC;
+           btn : in  STD_LOGIC_VECTOR (2 downto 0);
+           led : out  STD_LOGIC_VECTOR (2 downto 0));
+end state_machine;
+
+architecture Behavioral of state_machine is
+	type StateT is (st0, st1, st2, st3);
+	signal state : StateT := st0;
+	signal state_next : StateT;
+begin
+	clocked_process : process(clk)
+	begin
+		if rising_edge(clk) then
+			state <= state_next;
+		end if;
+	end process;
+	
+	state_machine : process(state, btn)
+	begin
+		state_next <= state;
+		case btn is
+			when "001" =>
+				state_next <= st1;
+			when "010" =>
+				state_next <= st2;
+			when "100" =>
+				state_next <= st3;
+			when others =>
+				null;
+		end case;
+	end process;
+	
+	with state select
+		led <= "000" when st0,
+		       "001" when st1,
+				 "010" when st2,
+				 "100" when st3,
+				 "111" when others;
+end Behavioral;
+
---
+++ exercise_2/task_1_d/test.vhd
@@ -0,0 +1,96 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   14:32:05 06/17/2012
+-- Design Name:   
+-- Module Name:   /home/live/workspace/pld/exercise_2/task_1/test.vhd
+-- Project Name:  task_1
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: state_machine
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY test IS
+END test;
+ 
+ARCHITECTURE behavior OF test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT state_machine
+    PORT(
+         clk : IN  std_logic;
+         btn : IN  std_logic_vector(2 downto 0);
+         led : OUT  std_logic_vector(2 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal btn : std_logic_vector(2 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal led : std_logic_vector(2 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: state_machine PORT MAP (
+          clk => clk,
+          btn => btn,
+          led => led
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin
+
+      wait for clk_period;
+		btn <= "001";
+      wait for clk_period;
+		btn <= "010";
+      wait for clk_period;
+		btn <= "111";
+		wait for clk_period;
+		btn <= "100";
+
+      wait;
+   end process;
+
+END;
